2e84f39dee4152fdc5fd7afecb47040045ea13e4
Nizaam <nizaams@gmail.com>
2026-09-03 19:00:21 +0200

    scaffolding

---
+++ src/MyScheduler.Api/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using MyScheduler.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+app.MapHealthChecks("/health");
+
+app.Run();
+
+public partial class Program
+{
+}
---
+++ tests/MyScheduler.Api.IntegrationTests/HealthCheckTests.cs
@@ -0,0 +1,24 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace MyScheduler.Api.IntegrationTests;
+
+public class HealthCheckTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public HealthCheckTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task Health_ReturnsOk()
+    {
+        var client = _factory.CreateClient();
+
+        var response = await client.GetAsync("/health");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
